0fb7d44f6fe057ce3d5eeeecef92556a6e40fb5b
Daniel Gould <dmgould310@gmail.com>
2020-08-27 18:44:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -33,7 +33,8 @@ class Dog
       VALUES (?,?)
      );  
     SQL
-    hello = DB[:conn].execute(sql, self.name, self.breed)  
+    DB[:conn].execute(sql, self.name, self.breed)  
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
   end 
   
   
