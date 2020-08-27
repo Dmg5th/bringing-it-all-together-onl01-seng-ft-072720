1c75773ac2a17d162728c08cbfa8103f7cac3145
Daniel Gould <dmgould310@gmail.com>
2020-08-27 18:31:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,12 +3,15 @@ class Dog
   attr_accessor :name, :breed 
   attr_reader :id
   
-  def initialize(id=nil, name:, breed:) 
+  def initialize(id=nil, name, breed) 
     @id = id 
     @name = name 
     @breed = breed 
    
   end 
+  
+  
+
   
   
 end 
