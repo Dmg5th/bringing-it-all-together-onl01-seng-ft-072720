098117c822b3aa8374cb5cb2b85ad538a8f4e8a0
Daniel Gould <dmgould310@gmail.com>
2020-08-27 18:40:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -28,6 +28,15 @@ class Dog
   end 
   
   def save
+    sql = <<-SQL 
+      CREATE TABLE dogs (
+      id INTEGER PRIMARY KEY , 
+      name TEXT, 
+      breed TEXT 
+      );  
+    SQL
+    DB[:conn].execute(sql)  
+  end 
   
   
 
